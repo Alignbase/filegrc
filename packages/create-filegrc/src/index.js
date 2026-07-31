b1e9dd42653dcc4bec9f9bcae758900b9bfa6661
Abe Wheeler <abe.wheeler3@gmail.com>
2026-07-31 14:46:16 -0500

    Release v0.3.3

--- packages/create-filegrc/src/index.js
+++ packages/create-filegrc/src/index.js
@@ -452,13 +452,13 @@ async function runCombinedSetup(target, input) {
 async function writeMinimalLockfile(target, name, versionRange) {
   const lock = {
     name,
-    version: "0.3.2",
+    version: "0.3.3",
     lockfileVersion: 3,
     requires: true,
     packages: {
       "": {
         name,
-        version: "0.3.2",
+        version: "0.3.3",
         dependencies: { filegrc: versionRange }
       }
     }
